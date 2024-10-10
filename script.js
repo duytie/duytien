94a72d963bf1e6775c279cb6aa1118ac58c25c8e
duytien <147395987+duytie@users.noreply.github.com>
2024-10-11 00:40:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const noBtn = document.querySelector(".no-btn");
 const questionPrompt = document.querySelector(".question-prompt"); // Thêm đoạn này
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "hehe a biết là e sẽ tha lỗi cho a mà iu em 💕💕💕💕";
+  question.innerHTML = "tuyệt quá e đồng ý rồi 😁😁😁😁";
   gif.src =
-    "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnRhcXNpd2F1dG4wbnFobXNiOXdnOXJvZno2cnc2Y24ybzMxaWpvYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3EiNuzEYidMZoMDjwx/giphy.gif";
+    "https://tenor.com/vi/view/dancing-cat-dance-cat-cat-meme-chinese-cat-gif-7364280193643012451";
 
   // Ẩn cả hai nút Yes và No
   yesBtn.style.display = "none";
